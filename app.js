1518520272874c1625280980834f8dc123aa2ebb
divya <goyaldivya53@gmail.com>
2019-03-30 17:17:27 +0530

    changed some messages in app(dot)js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.load({ path: '.env' });
 
 const bot = new Telegraf(process.env.TELEBOT_JINTECH);
 
-const commonText = `Jai Jinendra!\n\nWelcome to JinSwara bot.\n\nMessage in following format to get the definition of any Jainism Terminology.\n\n/define केवलज्ञान`;
+const commonText = `Jai Jinendra!\n\nWelcome to JinSwara bot.\n\nMessage in following format to get the definition of any Jainism Terminology.\n\n/define केवलज्ञान\n\n Type /about for more info.\n\nType \\ask to ask a question.\n\nType \contact to connect to the team.`;
 
 bot.start((req) => req.reply(commonText));
 
@@ -15,11 +15,11 @@ bot.command('about', (req) => {
 });
 
 bot.command('ask', (req) => {
-    req.reply("Please visit forum.jinswara.com");
+    req.reply("Please visit forum.jinswara.com to ask Jainism related questions.");
 });
 
 bot.command('contact', (req) => {
-    req.reply("Please contact connect@jinswara.com");
+    req.reply("Please contact connect@jinswara.com or join us https://t.me/JinSwara");
 });
 
 bot.command('define', (req) => {
@@ -27,7 +27,7 @@ bot.command('define', (req) => {
     if (define[term]) {
         req.reply(define[term]);
     } else {
-        req.reply(`The term ${term} is stored in our database yet.`);
+        req.reply(`The term ${term} is not yet defined in our database. Please contact our team at connect@jinswara.com`);
     }
 });
 
